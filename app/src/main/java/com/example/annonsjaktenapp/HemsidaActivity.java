dbaed797deb047f16c38a4cb716498e0a04e97a1
Cagri Cimen <cagricode@gmail.com>
2023-03-22 15:45:20 +0100

    Added buttons to change pages.

--- app/src/main/java/com/example/annonsjaktenapp/HemsidaActivity.java
+++ app/src/main/java/com/example/annonsjaktenapp/HemsidaActivity.java
@@ -2,7 +2,9 @@ package com.example.annonsjaktenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HemsidaActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class HemsidaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hemsida);
+    }
+
+    public void goToFiltreringsSida(View view) {
+        Intent intent = new Intent(this, FiltreringSidaActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToLoginSida(View view) {
+        Intent intent = new Intent(this, InloggningssidaActivity.class);
+        startActivity(intent);
     }
 }
