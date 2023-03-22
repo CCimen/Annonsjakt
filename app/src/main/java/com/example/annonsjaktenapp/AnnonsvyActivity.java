d1ef1536aadb5a6feb8421133065d475f6395f23
Cagri Cimen <cagricode@gmail.com>
2023-03-22 13:50:34 +0100

    First commit 22 mars

---
+++ app/src/main/java/com/example/annonsjaktenapp/AnnonsvyActivity.java
@@ -0,0 +1,14 @@
+package com.example.annonsjaktenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnnonsvyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_annonsvy);
+    }
+}
