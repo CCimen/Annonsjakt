a988c02ed4b39b76c7a76b6a0c23762a209cbcbd
CCimen <cagri.cimen.3264@student.uu.se>
2023-05-03 18:54:14 +0200

    Fixed recommended products

--- app/src/main/java/com/example/annonsjaktenapp/HemsidaActivity.java
+++ app/src/main/java/com/example/annonsjaktenapp/HemsidaActivity.java
@@ -2,7 +2,6 @@ package com.example.annonsjaktenapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 public class HemsidaActivity extends BaseActivity {
@@ -13,11 +12,22 @@ public class HemsidaActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hemsida);
 
-        // Variabler för att länka bilderna till kategorivyn
+        // Variables for linking the images to the category view
         ImageView categoryImage1 = findViewById(R.id.category_image1);
         ImageView categoryImage2 = findViewById(R.id.category_image2);
         ImageView categoryImage3 = findViewById(R.id.category_image3);
         ImageView categoryImage4 = findViewById(R.id.category_image4);
+
+        // Variables for linking the images to recommended products
+        ImageView productImage1 = findViewById(R.id.product_image1);
+        ImageView productImage2 = findViewById(R.id.product_image2);
+        ImageView productImage3 = findViewById(R.id.product_image3);
+        ImageView productImage4 = findViewById(R.id.product_image4);
+
+        productImage1.setOnClickListener(v -> showProductDetails(1, 0));
+        productImage2.setOnClickListener(v -> showProductDetails(1, 1));
+        productImage3.setOnClickListener(v -> showProductDetails(2, 4));
+        productImage4.setOnClickListener(v -> showProductDetails(4, 3));
 
         setUpBottomNavigation();
 
@@ -44,73 +54,12 @@ public class HemsidaActivity extends BaseActivity {
             intent.putExtra("selected_category", 4);
             startActivity(intent);
         });
-
-        // Variabler för att länka bilderna till annonsvyn
-        ImageView productImage1 = findViewById(R.id.product_image1);
-        ImageView productImage2 = findViewById(R.id.product_image2);
-        ImageView productImage3 = findViewById(R.id.product_image3);
-        ImageView productImage4 = findViewById(R.id.product_image4);
-
-        productImage1.setOnClickListener(v -> {
-            Intent intent = new Intent(HemsidaActivity.this, AnnonsvyActivity.class);
-            intent.putExtra("product_image_resource", R.drawable.datorproduct1);
-            startActivity(intent);
-        });
-
-        productImage2.setOnClickListener(v -> {
-            Intent intent = new Intent(HemsidaActivity.this, AnnonsvyActivity.class);
-            intent.putExtra("product_image_resource", R.drawable.datorproduct2);
-            startActivity(intent);
-        });
-
-        productImage3.setOnClickListener(v -> {
-            Intent intent = new Intent(HemsidaActivity.this, AnnonsvyActivity.class);
-            intent.putExtra("product_image_resource", R.drawable.skonhetproduct5);
-            startActivity(intent);
-        });
-
-        productImage4.setOnClickListener(v -> {
-            Intent intent = new Intent(HemsidaActivity.this, AnnonsvyActivity.class);
-            intent.putExtra("product_image_resource", R.drawable.product_image4);
-            startActivity(intent);
-        });
-
-        // OnClickListener för rekommenderade produkter
-        View.OnClickListener productClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int index = -1;
-                switch (v.getId()) {
-                    case R.id.product_image1:
-                        index = 0;
-                        break;
-                    case R.id.product_image2:
-                        index = 1;
-                        break;
-                    case R.id.product_image3:
-                        index = 2;
-                        break;
-                    case R.id.product_image4:
-                        index = 3;
-                        break;
-                }
-                if (index != -1) {
-                    openItemDetails(recommendedProductIds[index]);
-                }
-            }
-        };
-
-        productImage1.setOnClickListener(productClickListener);
-        productImage2.setOnClickListener(productClickListener);
-        productImage3.setOnClickListener(productClickListener);
-        productImage4.setOnClickListener(productClickListener);
     }
 
-
-    // Uppdatera openItemDetails-metoden
-    private void openItemDetails(String productId) {
-        Intent intent = new Intent(HemsidaActivity.this, item_detailsActivity.class);
-        intent.putExtra("product_id", productId);
+    private void showProductDetails(int categoryId, int itemIndex) {
+        Intent intent = new Intent(this, item_detailsActivity.class);
+        intent.putExtra("category_id", categoryId);
+        intent.putExtra("item_index", itemIndex);
         startActivity(intent);
     }
 }
